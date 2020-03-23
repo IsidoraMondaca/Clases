f85254c88d5882094865b274605dd73520ba329a
IsidoraMondaca <mondaca.isidora@gmail.com>
2020-03-23 15:22:56 -0300

    Cambios Clase 2

--- Class_02/Script_Clase02.R
+++ Class_02/Script_Clase02.R
@@ -14,39 +14,64 @@
 
 # Creación de Objetos
 
-x<-NULL
-y<-c(TRUE,FALSE)
-as.numeric(y)
+x<-NULL #si es un objeto (en blanco)
+y<-c(TRUE,FALSE) #dos elementos concatenados con la función c (elementos lógicos)
+as.numeric(y) #convertir a numérico (si es que se puede)
 
 A<-1
 years<-2010:2020
+year<-seq(2010,2020,by = 0.5)
 tiktoc<-c("Que", "linda", "te ves", "limpiando", "Esperancita")
+#c("Que", "linda", "te ves", "limpiando", "Esperancita",4) si corro esto, R fuerza a 4 a ser del mismo tipo que los demás
+
+paste("hola","mundo",sep=" ") #el sep es como quiero que separe
+paste(tiktoc,collapse=" ")
+
+obj2<-c(1,2,3,4,"Esperancita") #transforma los demás en string
+as.numeric(obj2)
+
+class(tiktoc)
+numeros_en_texto <-c("1","2","3")
+as.numeric(numeros_en_texto)
+
 m1<-matrix(1:4,2,2)
-a1<-array(1:12,dim = c(2,2,3))
+m1%*%t(m1) #multiplicó m1 por su traspuesta
+diag(m1)
+solve(m1) #da la inversa de la matriz
+
+a1<-array(1:12,dim = c(2,2,3)) #tiene 3 dimensiones
+
 d1<-data.frame(m1)
-data("quakes") # promise
+data("quakes") # promise #quakes es una base ya cargada en R
 d1<-data.frame(quakes)
+View(d1) #abre la tabla de datos
 
 ls()
-l1<-list(A=A,years,tiktoc,m1)
+l1<-list(NumeroUno=A,years,tiktoc,m1) #listas objetos muy complejos, puede tener cualquier cosa
 
 # Manipulación de Objetos
 ls()
 
 class(A)
-typeof(A)
+typeof(A) #si los elementos dentro de qué tipo son (me sale doble pq tiene decimal)
+
+A <- 1L
+class(A)
+typeof(A) 
 
 length(years)
 dim(m1)
-object.size(d1)
+length(m1) #como es un objeto de 2 dim solamente suma
+object.size(d1) #indica los bytes
 
-names(d1)
-head(d1)
-tail(d1)
+names(d1) #nombres de las bases de datos
+head(d1) #6 primeras observaciones de la base de datos
+tail(d1) #6 últimas observaciones
 
-rm(l1)
+rm(A)
 
 #Bonus: como se borra todo?
+rm(list=ls()) #elimina todo el ambiente
 
 # Indexación uso de los []
 
@@ -54,25 +79,39 @@ length(years)
 years[1]
 
 dim(m1)
-m1[2,3]
+m1[2,3] #segunda fila, tercera columna
+m1[1,2] #cuando hay más dimensiones, uso comas
 
 dim(a1)
-a1[2,1,3]
+a1[2,1,3] #segunda fila, primera columa, layer 3
 
 l1[2]
-l1[2][[1]][1:2]
+l1[2][[1]][1:2] #se sigue una lógica de encadenamiento
+class(l1[2])
+class(l1[2][[1]])
+class(years)
 
-l1[[2]][1:2]
+l1[[2]][3:5]
 
-d1[1,]
-d1[,1]
+l1$NumeroUno
+
+d1[1,] #d1 es de dos dimensiones, 1000 y 5
+d1[,1] #valores de la primera columna
+d1[,"lat"]
+d1$mag[1:5]
+d1$mag[-1] #todos menos el primero
+d1$mag[c(1,3,5)]
+d1$mag[seq(1,16,2)]
 d1$lat[1:4]
+
 d1[,'lat']
 d1[1:4,c('lat','long')]
-d1$mag>5
+d1$mag>5 #llamo el objeto con una condición
 table(d1$mag>5)
-d1[d1$mag>6,]
-d1$dummy_5up<-as.numeric(d1$mag>5)
+d1[d1$mag>6,] #me lo entrega en formato tabla 
+d1[d1$mag>6,"stations"]
+
+d1$dummy_5up<-as.numeric(d1$mag>5) #se creó una variable dummy 
 head(d1)
 
 # Distinguir entre funciones, objetos, números y sintaxis básica
@@ -91,17 +130,18 @@ if(A==1){
   A<-1L
 }
 
+#for loop
 
 for(i in 1:5){
   print(paste("Me le declaro a la ", i))
-  Sys.sleep(2)
+  Sys.sleep(2) #se duerme el sistema por 2 segundos
   print("no mejor no... fail!")
   Sys.sleep(1)
 }
 
-i<-1
+i<-1 #NO ME FUNCIONO
+eps<-50/(i^2)
 while(eps>0.001){
-  eps<-50/(i^2)
   print(paste("eps value es still..", eps))
   i<-i+1
 }
@@ -114,15 +154,16 @@ while(eps>0.001){
 
 quakes[quakes$mag>6,'mag']
 
-by(data = quakes$mag,INDICES = quakes$stations,FUN = mean)
+by(data = quakes$mag,INDICES = quakes$stations,FUN = mean) #sacar el promedio demagnitud por estación
 tapply(X = quakes$mag,INDEX = quakes$stations, FUN = mean)
 
 ### 2. tydiverse 
 #https://rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
+install.packages("tidyverse")
 library(tidyverse)
 #Cómo se instala el paquete si no lo tengo? Tank!!! ayudaaaa!
 
-quakes %>% 
+quakes %>% #%>% sirve para ir filtrando
   filter(mag>6) %>% 
   select(mag) 
 
@@ -137,9 +178,10 @@ library(data.table)
 
 quakes<-data.table(quakes)
 
-quakes[mag>6,.(mag)]
+quakes[mag>6,.(mag)] #.() sirve para hacer listas
+quakes[mag>6,.(mag,depth)]
 
-quakes[,mean(mag),by=.(stations)]
+quakes[,mean(mag),by=.(stations)] #agrega una tercera dimensión
 
 ### Reading data from a file
 
